0c00f562ca30520b8f51fce3b95c7c2882dd2dcd
Leonardo Pereira Santos <leonardo.santos@restinga.ifrs.edu.br>
2016-05-01 19:50:19 -0300

    Added support for all ldr and std modes (register, offset, pre and post-indexed).

--- src/sim_memory.cc
+++ src/sim_memory.cc
@@ -71,13 +71,19 @@ int memory::fill(std::ifstream &infile) {
 	string line;
 	smatch base_match;
 
-	regex org_regex("^[ \t]*org:[ \t]+0x([0-9a-fA-F]+)", std::regex_constants::egrep);
-//	regex movimm_regex("^[ \t]*mov[ \t]+[rR]([0-9]+),[ \t]*#([0-9])+", std::regex_constants::egrep);
-	regex movimm_regex("^[ \t]*mov[ \t]+[rR]([0-9]+),[ \t]*(#([0-9])+|#0x([0-9a-fA-F]+))", std::regex_constants::egrep);
-	regex mov_regex("^[ \t]*mov[ \t]+[rR]([0-9]+),[ \t]*[rR]([0-9]+)", std::regex_constants::egrep);
+    regex comment_regex("^[ \t]*//", std::regex_constants::extended);
+	regex org_regex("^[ \t]*org:[ \t]+0x([0-9a-f]+)", std::regex_constants::extended);
+	regex movimm_regex("^[ \t]*mov[ \t]+r([0-9]+),[ \t]*(#([0-9])+|#0x([0-9a-f]+))", std::regex_constants::extended);
+	regex mov_regex("^[ \t]*mov[ \t]+r([0-9]+),[ \t]*r([0-9]+)", std::regex_constants::extended);
+	regex ldr_reg_regex("^[ \t]*(ldr|str)[ \t]*r([0-9]+),[ \t]*\\[r([0-9]+)\][ \t]*$", std::regex_constants::extended);
+	regex ldr_off_regex("^[ \t]*(ldr|str)[ \t]*r([0-9]+),[ \t]*\\[r([0-9]+),[ \t]*#(([-0-9]+)|([-0]+x[0-9a-f]+))\][ \t]*$", std::regex_constants::extended);
+	regex ldr_pre_regex("^[ \t]*(ldr|str)[ \t]*r([0-9]+),[ \t]*\\[r([0-9]+),[ \t]*#(([-0-9]+)|([-0]+x[0-9a-f]+))\]( !|!)[ \t]*$", std::regex_constants::extended);
+	regex ldr_post_regex("^[ \t]*(ldr|str)[ \t]*r([0-9]+),[ \t]*\\[r([0-9]+)\],[ \t]*#(([-0-9]+)|([-0]+x[0-9a-f]+))[ \t]*$", std::regex_constants::extended);
 
 	while( getline(infile,line) ) {
-		if( regex_match(line, base_match, org_regex)) {
+		 if(regex_search (line, comment_regex)) {
+			 continue;
+		} else if( regex_match(line, base_match, org_regex)) {
 			if( base_match.size() == 2 ) {
 				curr_addr = stol(base_match[1].str(), nullptr, 16);
 				cout << "curr_addr = " << curr_addr << endl;
@@ -87,18 +93,54 @@ int memory::fill(std::ifstream &infile) {
 				cout << "MOV r" << stoi(base_match[1].str()) << ", r" << stoi(base_match[2].str()) << endl;
 			}
 		} else if( regex_match(line, base_match, movimm_regex)) {
-			cout << line << " : base_match.size() = " << base_match.size() << endl;
-			for (int i = 0; i < base_match.size(); ++i) {
-				cout << "base_match[" << i << "].str() = " << base_match[i].str() << endl;
-			}
 			if( base_match.size() == 5 ) {
 				if( base_match[2].str()[2] == 'x' )
 					cout << "MOV r" << stoi(base_match[1].str()) << ", #0x" << stoi(base_match[4].str(), nullptr, 16) << endl;
 				else
 					cout << "MOV r" << stoi(base_match[1].str()) << ", #" << stoi(base_match[3].str()) << endl;
 			}
-		} else // regex_search
-			cout << "Not matched" << line << endl;
+		} else if(regex_match (line, base_match, ldr_reg_regex)) {
+//			cout << line << " : base_match.size() = " << base_match.size() << endl;
+//			for (unsigned int i = 0; i < base_match.size(); ++i) {
+//				cout << "base_match[" << i << "].str() = " << base_match[i].str() << endl;
+//			}
+			if( base_match.size() == 4 ) {
+				if( base_match[1].str() == "ldr" )
+					cout << "LDR r" << base_match[2].str() << ", [r" << base_match[3].str() << "]" << endl;
+				if( base_match[1].str() == "str" )
+					cout << "STR r" << base_match[2].str() << ", [r" << base_match[3].str() << "]" << endl;
+			}
+		} else if(regex_match (line, base_match, ldr_off_regex)) {
+			if( base_match.size() == 7 ) {
+				if( base_match[1].str() == "ldr" )
+					cout << "LDR r" << base_match[2].str() << ", [r" << base_match[3].str() << ", #" << base_match[4].str() << "]" << endl;
+				if( base_match[1].str() == "str" )
+					cout << "STR r" << base_match[2].str() << ", [r" << base_match[3].str() << ", #" << base_match[4].str() << "]" << endl;
+			}
+		} else if(regex_match (line, base_match, ldr_pre_regex)) {
+//			cout << "LDR pre-indexed offset : " << line << endl;
+			if( base_match.size() == 8 ) {
+				if( base_match[1].str() == "ldr" )
+					cout << "LDR r" << base_match[2].str() << ", [r" << base_match[3].str() << ", #" << base_match[4].str() << "]!" << endl;
+				if( base_match[1].str() == "str" )
+					cout << "STR r" << base_match[2].str() << ", [r" << base_match[3].str() << ", #" << base_match[4].str() << "]!" << endl;
+			}
+		} else if(regex_match (line, base_match, ldr_post_regex)) {
+//			cout << "LDR post-indexed offset : " << line << endl;
+//			cout << line << " : base_match.size() = " << base_match.size() << endl;
+//			for (unsigned int i = 0; i < base_match.size(); ++i) {
+//				cout << "base_match[" << i << "].str() = " << base_match[i].str() << endl;
+//			}
+			if( base_match.size() == 7 ) {
+				if( base_match[1].str() == "ldr" )
+					cout << "LDR r" << base_match[2].str() << ", [r" << base_match[3].str() << "], #" << base_match[4].str() << endl;
+				if( base_match[1].str() == "str" )
+					cout << "STR r" << base_match[2].str() << ", [r" << base_match[3].str() << "], #" << base_match[4].str() << endl;
+			}
+		} else {// regex_search (ldr_reg_regex, line)
+			if( line.size() > 0 )
+				cout << "Not matched" << line << endl;
+		}
 	}
 	return 0;
 }
